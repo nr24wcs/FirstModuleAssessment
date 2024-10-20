825a7d12a16b13c6f335ba63be81d7352e5886d7
nr24wcs <npr216@hotmail.com>
2024-10-20 13:26:50 -0400

    init commit

---
+++ first.py
@@ -0,0 +1,9 @@
+#first.py
+# Introduction about me
+name="Natty"
+age="40"
+summary="I want to learn about Python because I have always been fascinated by programming and computers. It will also give me more knowledge for my work."
+
+print(name)
+print(age)
+print(summary)
